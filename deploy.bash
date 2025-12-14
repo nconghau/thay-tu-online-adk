03965b7fe4f5de3469431e4013950833f81abdcd
macos <HAU2803NCH@gmail.com>
2025-12-14 14:55:20 +0700

    feat: Introduce deployment script and refine UI element heights for improved responsiveness.

---
+++ deploy.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ======== Script run ===========
+
+# python3 -m venv venv && source venv/bin/activate
+# pip install -r requirements.txt
+# python app.py
+
+# docker-compose up --build -d
+# git push -f origin main && git push -f space main 
+
+git push origin main && git push space main 
--- main.py
+++ main.py
@@ -4,12 +4,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# ======== Script run ===========
-
-# python3 -m venv venv && source venv/bin/activate
-# pip install -r requirements.txt
-# python app.py
-
-# docker-compose up --build -d
-# git push -f origin main && git push -f space main 
